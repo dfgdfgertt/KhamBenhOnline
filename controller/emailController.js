c8581cfdbae35ccf0d0971f08b041314bc27d164
dfgdfgertt <majinsyn@gmail.com>
2021-06-17 02:22:22 +0700

    update final

--- controller/emailController.js
+++ controller/emailController.js
@@ -1,40 +1,43 @@
 var nodemailer = require('nodemailer');
-const express = require('express');
+var smtpTransport = require('nodemailer-smtp-transport');
 
 let user = {
     user: 'thienhoach14141@gmail.com',
     pass: 'bidaica1@'
 }
 
-let transport = nodemailer.createTransport({
-    service: 'gmail',
+let transport = nodemailer.createTransport(smtpTransport({
+    service: 'Gmail',
     auth: {
        user: user.user,
        pass: user.pass
     }
-});
+}));
+
+console.log('SMTP Configured');
 
 const sendMailPayment = (book)=>{
   
     const message = {
-        from: 'thienhoach14141@gmail.com', // Sender address
+        from: 'LC Health <thienhoach14141@gmail.com>', // Sender address
         to: book.mail,         // List of recipients
         subject: '[LC Health] Thanh toán thành công', // Subject line
         html: 'Bạn đã thanh toán đặt lịch khám thành công với số tiền: ' + book.idOrder.price+'<br>Xem thông tin lịch khám ở <a href="lchealth.surge.sh/history-book-details/'+book._id+'">đây</a> ' // Plain text body
     };
     return transport.sendMail(message, function(err, info) {
         if (err) {
-          console.log(err)
+          console.log(err.message)
         } else {
           console.log(info);
         }
+        transport.close();
     });
 }
 
 
 const sendMailBooking = (book)=>{
     const message = {
-        from: 'thienhoach14141@gmail.com', // Sender address
+        from: 'LC Health <thienhoach14141@gmail.com>', // Sender address
         to: book.mail,         // List of recipients
         subject: '[LC Health] Đặt khám thành công', // Subject line
         html: 'Bạn đã đặt lịch khám tại LC health thành công vào lúc: ' + book.time + ' ngày: '+ book.day+'<br>Xem thông tin lịch khám ở <a href="lchealth.surge.sh/history-book-details/'+book._id+'">đây</a> '
@@ -42,25 +45,27 @@ const sendMailBooking = (book)=>{
     };
     return transport.sendMail(message, function(err, info) {
         if (err) {
-          console.log(err)
+          console.log(err.message)
         } else {
           console.log(info);
+          transport.close();
         }
     });
 }
 
 const senMailForgotPasswork = (otp,mail)=>{
     const message = {
-        from: 'thienhoach14141@gmail.com', // Sender address
+        from: 'LC Health <thienhoach14141@gmail.com>', // Sender address
         to: mail,         // List of recipients
         subject: '[LC Health] Lấy lại mật khẩu', // Subject line
         text: 'Mã đặt lại mật khẩu của bạn là: ' + otp// Plain text body
     };
     return transport.sendMail(message, function(err, info) {
         if (err) {
-          console.log(err)
+          console.log(err.message)
         } else {
           console.log(info);
+          transport.close();
         }
     });
 }
